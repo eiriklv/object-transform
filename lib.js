69b0ac5fcdaad28c7b0843d28f06ccd0c791aef6
Eirik Vullum <evullum@gmail.com>
2015-05-13 18:15:58 +0200

    use lodash and async-each

--- lib.js
+++ lib.js
@@ -1,32 +1,16 @@
-(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory();
-  } else {
-    root.returnExports = factory();
-  }
-}(this, function() {
+var clone = require('lodash.clonedeep');
+var get = require('lodash.get');
+var set = require('lodash.set');
+var asyncEach = require('async-each');
 
+module.exports = (function() {
   var objectTransform = {};
-
-  function _clone(object) {
-    return JSON.parse(JSON.stringify(object));
-  }
-
-  function _onlyOnce(fn) {
-    var called = false;
-    return function() {
-      if (called) throw new Error('callback was already called');
-      called = true;
-      fn.apply(null, arguments);
-    }
-  }
 
   function _addNestedProp(key, result, specs, input) {
     if (typeof specs[key] === 'string') {
-      result[key] = _getPropByString(specs[key], input)
-    } else if (typeof specs[key] === 'object' &&
+      result[key] = get(input, specs[key])
+    } else if (
+      typeof specs[key] === 'object' &&
       specs[key]
     ) {
       _addField(result, key);
@@ -60,9 +44,9 @@
     if (typeof specs[key] === 'function') {
       output.push({
         location: trail,
-        value: _getPropByString(trail, input),
+        value: get(input, trail),
         transform: specs[key]
-      })
+      });
     } else if (typeof specs[key] === 'object' &&
       specs[key]
     ) {
@@ -72,6 +56,7 @@
 
   function _addTransforms(output, specs, input, trail) {
     if (!specs) return;
+    
     trail = trail || '';
 
     try {
@@ -85,7 +70,7 @@
     });
   }
 
-    function _addNestedDerivativeCopy(key, result, specs, input) {
+  function _addNestedDerivativeCopy(key, result, specs, input) {
     if (Array.isArray(specs[key]) && typeof specs[key][0] === 'string') {
       result[key] = specs[key][0];
     } else if (typeof specs[key] === 'object' && specs[key]) {
@@ -132,13 +117,13 @@
   }
 
   function _getTransformSpec(derivatives, object) {
-    var transformSpec = _clone(derivatives);
+    var transformSpec = clone(derivatives);
     _addDerivativeTransform(transformSpec, derivatives, object);
     return transformSpec;
   }
 
   function _getCopySpec(derivatives, object) {
-    var copySpec = _clone(derivatives);
+    var copySpec = clone(derivatives);
     _addDerivativeCopy(copySpec, derivatives, object);
     return copySpec;
   }
@@ -148,77 +133,6 @@
     _addTransforms(output, specs, object);
     return output;
   };
-
-  function _getPropByString(s, o) {
-    s = s.replace(/\[(\w+)\]/g, '.$1');
-    s = s.replace(/^\./, '');
-
-    var a = s.split('.');
-
-    return (function iter() {
-      var n = a.shift();
-      var predicate = false;
-
-      try {
-        predicate = (n in o);
-      } catch (e) {}
-
-      if (!predicate) return;
-      o = o[n];
-
-      if (a.length)
-        return iter();
-      return o;
-    }());
-  }
-
-  function _setPropByString(s, u, o) {
-    s = s.replace(/\[(\w+)\]/g, '.$1');
-    s = s.replace(/^\./, '');
-
-    var a = s.split('.');
-
-    return (function iter(object) {
-      var n = a.shift();
-
-      if (!object[n]) {
-        object[n] = {};
-      }
-
-      if (a.length) return iter(object[n]);
-      object[n] = u;
-      return o;
-    }(o));
-  }
-
-  function _each(arr, iterator) {
-    for (var i = 0; i < arr.length; i += 1) {
-      iterator(arr[i], i, arr);
-    }
-  }
-
-  function _asyncEach(arr, iterator, callback) {
-    callback = callback || function() {};
-    if (!arr.length) {
-      return callback();
-    }
-    var completed = 0;
-    _each(arr, function(x) {
-      iterator(x, _onlyOnce(done));
-    });
-
-    function done(err) {
-      if (err) {
-        callback(err);
-        callback = function() {};
-      } else {
-        completed += 1;
-        if (completed >= arr.length) {
-          callback();
-        }
-      }
-    }
-  }
 
   objectTransform.transformSync = function(fn, object) {
     var keys = Object.keys(object);
@@ -232,17 +146,17 @@
   };
 
   objectTransform.copyToFrom = function(specs, object) {
-    var result = _clone(object);
+    var result = clone(object);
     _addProps(result, specs, object);
     return result;
   };
 
   objectTransform.transformToSync = function(transforms, object) {
     var transformList = _getTransformsList(transforms, object);
-    var output = _clone(object);
+    var output = clone(object);
 
     transformList.forEach(function(item) {
-      _setPropByString(item.location, item.transform(item.value), output);
+      set(output, item.location, item.transform(item.value));
     });
 
     return output;
@@ -252,7 +166,7 @@
     var keys = Object.keys(object);
     var result = {};
 
-    _asyncEach(keys, function(key, callback) {
+    asyncEach(keys, function(key, callback) {
       fn(result, object[key], key, callback);
     }, function(err) {
       callback(err, result);
@@ -261,12 +175,12 @@
 
   objectTransform.transformTo = function(transforms, object, callback) {
     var transformList = _getTransformsList(transforms, object);
-    var output = _clone(object);
+    var output = clone(object);
 
-    _asyncEach(transformList, function(item, callback) {
+    asyncEach(transformList, function(item, callback) {
       item.transform(item.value, function(err, result) {
         if (err) return callback(err);
-        _setPropByString(item.location, result, output);
+        set(output, item.location, result);
         callback();
       });
     }, function(err) {
@@ -279,7 +193,7 @@
       _getTransformSpec(derivatives),
       objectTransform.copyToFrom(
         _getCopySpec(derivatives),
-        _clone(object)
+        clone(object)
       )
     );
   };
@@ -289,12 +203,11 @@
       _getTransformSpec(derivatives),
       objectTransform.copyToFrom(
         _getCopySpec(derivatives),
-        _clone(object)
+        clone(object)
       ),
       callback
     );
   };
 
   return objectTransform;
-
-}));
+}());
